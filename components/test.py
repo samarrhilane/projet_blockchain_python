862992c74475a0ae1a270dc6ca07afc27d28d868
samarrhilane <samarrhilane@gmail.com>
2022-06-05 18:42:50 +0000

    Test comoponents

--- components/test.py
+++ components/test.py
@@ -2,11 +2,54 @@ from sys import setdlopenflags
 from ctypes import RTLD_GLOBAL
 setdlopenflags (RTLD_GLOBAL | 2)
 
-from members_component import Leg
-from animal_component import Pet
-p=Pet("medor chien")
-l=Leg("front left")
-l2=Leg("avant droite")
-p.addLeg(l)
-p.addLeg(l2)
-p.to_json()
+from sys import setdlopenflags
+from ctypes import RTLD_GLOBAL
+setdlopenflags(RTLD_GLOBAL | 2)
+
+import composant_cle
+import Signature_component
+
+cle = composant_cle.Cle()
+sign = Signature_component.Signature()
+
+init = cle.initialize("4b8e29b9b0dddd58a709edba7d6df6c07ebdaf5653e325114bc5318c238f87f0")
+Private_key = cle.getPrivateKey()
+Public_key = cle.getPublicKey()
+
+message = "hello"
+
+
+
+def CreateSignatur(message, Private_key) :
+	MySignature = sign.Message_Signature(message, Private_key)
+	return MySignature
+
+def VerifySignature(message, Public_key):
+	MySignature = CreateSignatur(message, Private_key)
+	Validate = sign.Signature_validation(message, Public_key, MySignature)
+	return Validate
+
+def VerifyLenSignature(Signature):
+	if((len(Signature) < 30) | (len(Signature)==0)):
+		print("Signature Length is not valid")
+	print("Signature Length is valid")
+
+def VerifyEmptySignature(Signature):
+	if(SIgnature==""):
+		print("Signature is Empty")
+
+
+def VerifyEmptySignature(message) :
+	if (message == "") :
+		print("Message is Empty")
+
+
+
+		
+Signature = sign.Message_Signature(message, Private_key)
+
+CreateSignatur(message, Private_key)
+VerifySignature(message, Public_key)
+VerifyLenSignature(Signature)
+VerifyEmptySignature(Signature)
+VerifyEmptySignature(message)
