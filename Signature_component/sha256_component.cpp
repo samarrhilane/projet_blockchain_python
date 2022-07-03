12c6e3b6f3aab45b27cc428851d49579db9ac762
samarrhilane <samarrhilane@gmail.com>
2022-07-03 13:50:17 +0200

    Create sha256_component.cpp

---
+++ Signature_component/sha256_component.cpp
@@ -0,0 +1,34 @@
+#include "sha256/sha256/sha256.h"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <cstring>
+
+class component_Hash {
+    public:
+        component_Hash() {}
+        ~component_Hash() {}
+
+        std::string SHA256 (std::string input){
+            std::string result;
+            if(input.empty()) {
+                cout << "ERROR input is empty !!" <<endl;
+            }else {
+                result = sha256(input);
+
+            }
+            return result;
+        }
+};
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(component_Hash, m) {
+    py::class_<component_Hash>(m, "component_Hash")
+        .def(py::init())
+        .def("SHA256", &component_Hash::SHA256);
+
+}
