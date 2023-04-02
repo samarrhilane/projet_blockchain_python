7c3cd337ce91015a7b89c62342ccdd0a99a7f58d
samarrhilane <samarrhilane@gmail.com>
2023-04-02 15:12:43 +0200

    Update test.py

--- components/test.py
+++ components/test.py
@@ -14,7 +14,8 @@ import Signature_component
 cle = composant_cle.Cle()
 sign = Signature_component.Signature()
 
-init = cle.initialize("4b8e29b9b0dddd58a709edba7d6df6c07ebdaf5653e325114bc5318c238f87f0")
+
+init = cle.initialize("4b8e29b9b0dddd58a709edba7d6df6c07ebdaf5653e325114bc5318c238f87f0")# test initialize for diff key
 Private_key = cle.getPrivateKey()
 Public_key = cle.getPublicKey()
 
